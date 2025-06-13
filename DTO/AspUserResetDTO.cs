62a649ba7a2298bcdbfa64e42e4dec3bb462aa66
claraanjelitakd <claraanjelitakd@gmail.com>
2025-06-13 20:32:06 +0700

    Delete DTO directory

--- DTO/AspUserResetDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleRESTApi.DTO
-{
-    public class AspUserResetDTO
-    {
-        public string Email { get; set; } = string.Empty;
-        public string NewPassword { get; set; } = string.Empty;
-        public string ConfirmPassword { get; set; } = string.Empty;
-    }
-}
